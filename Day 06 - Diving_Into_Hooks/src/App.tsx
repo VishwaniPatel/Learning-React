ac87a3a1d6eac1816dd632bd9cea7f659fb5cd1c
Viral N Patel <viral.n.patel@1rivet.com>
2024-01-18 18:20:15 +0530

    feat: create  demo of useState and useEffect

---
+++ Day 06 - Diving_Into_Hooks/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+// import ChatBook from './pages/use-effect/compnents/chat-book/ChatBook'
+// import Counter from './pages/use-state/components/Counter'
+
+function App() {
+  /**
+   * components for use-effect
+   * <ChatBook/>,<UserForm/> and <UserList/>
+   */
+
+  /**
+   * components for use-state
+   * <Counter/> and </DisplayUser/>
+   */
+  
+  return (
+    <>
+      {/* <ChatBook/> */}
+      {/* <Counter/> */}
+    </>
+  )
+}
+
+export default App
